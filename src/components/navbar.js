fc8efdee0ffa25d85f56f09d90218d626457a734
Verner Keel <verner.keel@gmail.com>
2021-01-27 09:27:11 +0000

    Added navbar and Response Tap tracking

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,80 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { FaPhone, FaRegEnvelope } from 'react-icons/fa';
 
-const Navbar = () => {
-  return (
-    <div className='container'>
-      <nav className='navbar' role='navigation' aria-label='main navigation'>
-        <div id='navbarBasicExample' className='navbar-menu'>
-          <div className='navbar-start'>
-            <a className='navbar-item'>Home</a>
+const NavbarBurger = (props) => (
+  <div
+    onClick={props.toggleMenu}
+    className={`navbar-burger burger ${props.active ? 'is-active' : ''}`}
+  >
+    <span />
+    <span />
+    <span />
+  </div>
+);
+
+export default class MegaNav extends React.Component {
+  state = {
+    activeMenu: false,
+  };
+  toggleMenu = () => {
+    this.setState({
+      activeMenu: !this.state.activeMenu,
+    });
+  };
+
+  render() {
+    return (
+      <nav className='navbar has-background-white-bis has-shadow'>
+        <div className='container'>
+          <div className='navbar-brand'>
+            <NavbarBurger
+              active={this.state.activeMenu}
+              toggleMenu={this.toggleMenu}
+            />
           </div>
+          <div
+            className={`navbar-menu ${
+              this.state.activeMenu ? 'is-active' : ''
+            }`}
+          >
+            <div className='navbar-start'>
+              <div className='navbar-item is-hidden-desktop'></div>
+              {/*TODO: ADD URL*/}
+              <Link to={'/'} className='navbar-item'>
+                Home
+              </Link>
+            </div>
+            <div className='navbar-end has-background-white-bis'>
+              <div className='navbar-item'>
+                <div className='field is-grouped'>
+                  <p className='control'>
+                    <a
+                      className='button is-danger response-tap'
+                      onClick={'rTapClickToCall(432151)'}
+                    >
+                      <span className='icon'>
+                        <FaPhone className='is-size-5' />
+                      </span>
 
-          <div className='navbar-end'>
-            <div className='navbar-item'>
-              <div className='buttons'>
-                <a className='button is-primary'>
-                  <strong>Sign up</strong>
-                </a>
+                      <span className='rTapNumber432151'>0161 794 0088</span>
+                    </a>
+                  </p>
+
+                  <p className='control'>
+                    <a className='button is-primary' href='/'>
+                      <span className='icon'>
+                        <FaRegEnvelope />
+                      </span>
+                      <span>Send email</span>
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </nav>
-    </div>
-  );
-};
-
-export default Navbar;
+    );
+  }
+}
---
+++ src/html.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id='___gatsby'
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        {/*RESPONSE TAP tracking code START*/}
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+   var adiInit = "76225", adiRVO = true;
+   var adiFunc = null;
+   (function() {
+      var adiSrc = document.createElement("script"); adiSrc.type = "text/javascript";
+      adiSrc.async = true;
+      adiSrc.src = ("https:" == document.location.protocol ? "https://static-ssl" : "http://static-cdn")
+      	+ ".responsetap.com/static/scripts/rTapTrack.min.js";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(adiSrc, s);
+   })();`,
+          }}
+        />
+        {/*RESPONSE TAP tracking code END*/}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
