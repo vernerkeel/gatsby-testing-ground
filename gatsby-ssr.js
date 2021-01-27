d5b676339173b76dd487940ad033f677703f613f
Verner Keel <verner.keel@gmail.com>
2021-01-27 13:02:04 +0000

    Added ssr.js / Response Tap test 3

---
+++ gatsby-ssr.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import NavbarWrapper from './src/components/navbarWrapper';
+export const wrapPageElement = ({ element, props }) => (
+  <NavbarWrapper {...props}>{element}</NavbarWrapper>
+);
