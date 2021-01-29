a012e0b7ce84a52b607703b7319d233a2b7e7a90
Ferran Buireu <fbuireu@gmail.com>
2021-01-29 18:11:01 +0100

    Refactor from class-based component to a functional component

--- src/components/megamenu/navbar.js
+++ src/components/megamenu/navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Link } from 'gatsby';
+import React, { useEffect, useRef, useState } from 'react';
 import CallEmailBtns from './callEmailBtns';
 import ServicesMenu from './servicesMenu';
 
@@ -8,51 +8,50 @@ const NavbarBurger = (props) => (
     onClick={props.toggleMenu}
     className={`navbar-burger burger ${props.active ? 'is-active' : ''}`}
   >
-    <span />
-    <span />
-    <span />
+    <span/>
+    <span/>
+    <span/>
   </div>
 );
 
-export default class Navbar extends React.Component {
-  state = {
-    activeMenu: false,
-  };
-  toggleMenu = () => {
-    this.setState({
-      activeMenu: !this.state.activeMenu,
-    });
+const Navbar = ({ location }) => {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const fakeReference = useRef(null);
+  const toggleMenu = () => {
+    setActiveMenu(!activeMenu);
   };
 
-  render() {
-    return (
-      <nav className='navbar has-background-white-bis has-shadow'>
-        <div className='container'>
-          <div className='navbar-brand'>
-            <NavbarBurger
-              active={this.state.activeMenu}
-              toggleMenu={this.toggleMenu}
-            />
+  return (
+    <nav className='navbar has-background-white-bis has-shadow' ref={fakeReference}>
+      <div className='container'>
+        <div className='navbar-brand'>
+          <NavbarBurger
+            active={activeMenu}
+            toggleMenu={toggleMenu}
+          />
+        </div>
+        <div
+          className={`navbar-menu ${
+            activeMenu ? 'is-active' : ''
+          }`}
+        >
+          <div className='navbar-start'>
+            <Link to={'/'} className='navbar-item' href='/'>
+              Home
+            </Link>
+            <ServicesMenu location={location}/>
           </div>
-          <div
-            className={`navbar-menu ${
-              this.state.activeMenu ? 'is-active' : ''
-            }`}
-          >
-            <div className='navbar-start'>
-              <Link to={'/'} className='navbar-item' href='/'>
-                Home
-              </Link>
-              <ServicesMenu />
-            </div>
-            <div className='navbar-end has-background-white-bis'>
-              <div className='navbar-item'>
-                <CallEmailBtns />
-              </div>
+          <div className='navbar-end has-background-white-bis'>
+            <div className='navbar-item'>
+              <CallEmailBtns/>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+
+};
+
+export default Navbar;
+
